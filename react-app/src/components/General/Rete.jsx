9ac3b21c61ad6bb1a37bc7e51b8fcab3413d9084
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-01-26 22:52:00 +0100

    ggtt

--- react-app/src/components/General/Rete.jsx
+++ react-app/src/components/General/Rete.jsx
@@ -1,86 +1,114 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import { Pen, PenFill } from "react-bootstrap-icons";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
+import { fetchData } from "../../redux/functions/fetch";
+import { TokenCommenti } from "../../token";
+import { setdataFetchGetCommenti } from "../../redux/reducers/StateSliceReducers";
 
 const Rete = () => {
+    const dispatch = useDispatch();
+
+    const optionsGetCommenti = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${TokenCommenti} `,
+        },
+    };
+
+    useEffect(() => {
+        dispatch(
+            fetchData(
+                "https://striveschool-api.herokuapp.com/api/comments/",
+                "",
+                optionsGetCommenti,
+                setdataFetchGetCommenti
+            )
+        );
+    }, []);
+
     const elencoCommenti = useSelector((state) => state.FetchData.dataFetchGetCommenti);
     const [show, setShow] = useState(false);
     const [datiModale, setDatiModale] = useState(null);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-    console.log(elencoCommenti);
+    console.log("elencoCommenti", elencoCommenti);
 
     return (
-        <div className="mt-7 w-100">
-            <div className="d-flex justify-content-center">
-                {" "}
-                <h2>Elenco dei commenti </h2>{" "}
-            </div>
-            {elencoCommenti.slice(0, 20).map((commento) => (
-                <Container>
-                    <Row>
-                        <Col>
-                            <div className="d-flex align-items-center">
-                                <div className="d-block  ">
-                                    <div className="d-flex gap-3 w-100 p-3 ">
-                                        <p className="m-0 my-1">{commento.author}</p>
-                                        <p className="m-0 my-1">{commento.comment}</p>
-                                        <p className="m-0 my-1">{commento.rate} 🌼 </p>
+        <>
+            {elencoCommenti && (
+                <div className="mt-7 w-100">
+                    <div className="d-flex justify-content-center">
+                        {" "}
+                        <h2>Elenco dei commenti </h2>{" "}
+                    </div>
+                    {elencoCommenti.slice(0, 20).map((commento) => (
+                        <Container>
+                            <Row>
+                                <Col>
+                                    <div className="d-flex align-items-center">
+                                        <div className="d-block  ">
+                                            <div className="d-flex gap-3 w-100 p-3 ">
+                                                <p className="m-0 my-1">{commento.author}</p>
+                                                <p className="m-0 my-1">{commento.comment}</p>
+                                                <p className="m-0 my-1">{commento.rate} 🌼 </p>
+                                            </div>
+                                        </div>
+                                        <Button
+                                            onClick={() => {
+                                                handleShow(true);
+                                                setDatiModale(commento);
+                                            }}
+                                            variant="transparent"
+                                        >
+                                            <div>
+                                                <PenFill fontSize={"25"} />
+                                            </div>
+                                        </Button>
                                     </div>
-                                </div>
-                                <Button
-                                    onClick={() => {
-                                        handleShow(true);
-                                        setDatiModale(commento);
-                                    }}
-                                    variant="transparent"
-                                >
-                                    <div>
-                                        <PenFill fontSize={"25"} />
-                                    </div>
+                                </Col>
+                            </Row>
+                        </Container>
+                    ))}
+                    {/* MODALE MESSO FUORI DAL MAP!! SOLO UN MODALE PER TUTTI QUANTI, IL MODALE VIENE CARICATO OGNI VOLTA CON I DATI DELL SINGOLO OGGETTO MAPPATO (commento), TRAMITE UNO STATO (datiModale) */}
+                    <div>
+                        <Modal show={show} onHide={handleClose}>
+                            <Modal.Header closeButton>
+                                <Modal.Title>uN MODALE VALIDO PER TUTTI</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <Form>
+                                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                        <Form.Label>Autore del commento</Form.Label>
+                                        <Form.Control type="text" /*  value={null || datiModale.author} */ />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                                        <Form.Label>commento</Form.Label>
+                                        <Form.Control type="text" /* value={null || datiModale.comment} */ />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                                        <Form.Label>valutazione in fiorellini</Form.Label>
+                                        <Form.Control type="text" /* value={null || datiModale.rate} */ />
+                                    </Form.Group>
+                                </Form>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button variant="secondary" onClick={handleClose}>
+                                    Close
                                 </Button>
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-            ))}
-            {/* MODALE MESSO FUORI DAL MAP!! SOLO UN MODALE PER TUTTI QUANTI, IL MODALE VIENE CARICATO OGNI VOLTA CON I DATI DELL SINGOLO OGGETTO MAPPATO (commento), TRAMITE UNO STATO (datiModale) */}
-            <div>
-                <Modal show={show} onHide={handleClose}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>uN MODALE VALIDO PER TUTTI</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <Form>
-                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                                <Form.Label>Autore del commento</Form.Label>
-                                <Form.Control type="text" autoFocus value={datiModale.author} />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                                <Form.Label>commento</Form.Label>
-                                <Form.Control type="text" autoFocus value={datiModale.comment} />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                                <Form.Label>valutazione in fiorellini</Form.Label>
-                                <Form.Control type="text" autoFocus value={datiModale.rate} />
-                            </Form.Group>
-                        </Form>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={handleClose}>
-                            Close
-                        </Button>
-                        <Button variant="primary" onClick={handleClose}>
-                            Save Changes
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
-            </div>
-        </div>
+                                <Button variant="primary" onClick={handleClose}>
+                                    Save Changes
+                                </Button>
+                            </Modal.Footer>
+                        </Modal>
+                    </div>
+                </div>
+            )}
+        </>
     );
 };
 

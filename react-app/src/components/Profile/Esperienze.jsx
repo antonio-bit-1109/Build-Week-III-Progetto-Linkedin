ff6c384f301677722d866d553cff0b04196a6030
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-01-27 12:33:28 +0100

    post esperienze

--- react-app/src/components/Profile/Esperienze.jsx
+++ react-app/src/components/Profile/Esperienze.jsx
@@ -5,8 +5,10 @@ import { Token } from "../../token";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchData } from "../../redux/functions/fetch";
 import { setDataFetchEsperienze } from "../../redux/reducers/StateSliceReducers";
-import Modale from "./Modale";
+import { fetchDataPost } from "../../redux/functions/fetchPost";
 import { fetchDelete } from "../../redux/functions/fetchDelete";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
 
 const URL = "https://striveschool-api.herokuapp.com/api/profile/";
 
@@ -30,52 +32,37 @@ const Esperienze = (props) => {
     const { userid } = props;
     const dispatch = useDispatch();
     const datiFetchEsperienze = useSelector((state) => state.FetchData.dataFetchEsperienze);
-    /*    const [isModalVisible, setIsModalVisible] = useState([]); */
-    /*  const [me, setMe] = useState(null); */
+    const [show, setShow] = useState(false);
+    const [esperienzaData, setEsperienzaData] = useState({
+        role: "",
+        company: "",
+        startDate: "",
+        endDate: "", // può essere null
+        description: "",
+        area: "",
+    });
 
-    /*     useEffect(() => {
-        if (props.me) {
-            setMe(true);
-        }
-    }, [props.me]); */
+    console.log("userid", userid);
 
-    // console.log("datiFetchEsperienze", datiFetchEsperienze);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     useEffect(() => {
         dispatch(fetchData(URL, `${userid}/experiences`, optionsGet, setDataFetchEsperienze));
     }, [userid, dispatch]);
 
-    /*   useEffect(() => {
-    if (datiFetchEsperienze) {
-      const arrayOfFalse = [];
-      for (let i = 0; i <= datiFetchEsperienze.length; i++) {
-        arrayOfFalse.push(false);
-      }
-      setIsModalVisible(arrayOfFalse);
-    }
-  }, [datiFetchEsperienze]); */
-
-    /*   const handleSetVisible = (n) => {
-    if (n === -1) {
-      let array = [...isModalVisible];
-      array[-1] = true;
-      setIsModalVisible(array);
-    } else if (n === false) {
-      let array = [...isModalVisible];
-      const arrayOfFalse = array.map(() => false);
-      setIsModalVisible(arrayOfFalse);
-    } else {
-      let array = [...isModalVisible];
-      array[n] = true;
-      setIsModalVisible(array);
-    }
-  }; */
-
     const handleDelete = async (expId) => {
         await fetchDelete(optionsDelete, userid, expId);
-        setTimeout(() => {
-            dispatch(fetchData(URL, `${userid}/experiences`, optionsGet, setDataFetchEsperienze));
-        }, 500);
+        dispatch(fetchData(URL, `${userid}/experiences`, optionsGet, setDataFetchEsperienze));
+    };
+
+    const HandleMakeAPost = async (event) => {
+        event.preventDefault();
+        await fetchDataPost(
+            `https://striveschool-api.herokuapp.com/api/profile/${userid}/experiences `,
+            esperienzaData
+        );
+        dispatch(fetchData(URL, `${userid}/experiences`, optionsGet, setDataFetchEsperienze));
     };
 
     return (
@@ -92,10 +79,8 @@ const Esperienze = (props) => {
                                 /* me &&  */ <div className="rounded-pill hover ">
                                     <Button variant="secondary-outline" /* onClick={() => handleSetVisible(-1)} */>
                                         {" "}
-                                        <PlusLg></PlusLg>{" "}
+                                        <PlusLg onClick={handleShow} />
                                     </Button>
-                                    <Modale /* userid={userid} show={isModalVisible[-1]} onHide={() => handleSetVisible(false)}  */
-                                    />
                                 </div>
                             }
                         </div>
@@ -111,19 +96,9 @@ const Esperienze = (props) => {
                                 datiFetchEsperienze.map((esperienza, i) => (
                                     <Col key={`key-${esperienza._id}`} className="my-1 border rounded-3" xs={12}>
                                         {" "}
-                                        <Modale
-                                            userid={userid}
-                                            /*  show={isModalVisible[i]} */
-                                            /* onHide={() => handleSetVisible(false)} */
-                                            esperienza={esperienza}
-                                        />
                                         <div className="d-flex gap-2 align-items-center">
                                             {
-                                                /* me ? */ /* (
-                                                <Button variant="outline-dark" className="add-image">
-                                                    <PlusLg />
-                                                </Button>
-                                            ) : */ <img
+                                                <img
                                                     className="rounded"
                                                     width={"40px"}
                                                     src="https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM="
@@ -176,13 +151,7 @@ const Esperienze = (props) => {
                                                 </Button>
                                             }
                                             {
-                                                /* me &&  */ <Button
-                                                    /* onClick={() => {
-                            handleSetVisible(i);
-                          }} */
-                                                    variant="light"
-                                                    className="rounded-pill px-3"
-                                                >
+                                                <Button variant="light" className="rounded-pill px-3">
                                                     <div className="rounded-pill hover ">
                                                         <Pencil></Pencil>
                                                     </div>
@@ -191,6 +160,129 @@ const Esperienze = (props) => {
                                         </div>
                                     </Col>
                                 ))}
+                            {/* MODALE VA FUORI DAL MAP, SOLO UN MDALE CARICATO CON I DATI, I DATI VENGONO SALVATI IN UNO STATO APPOSITO  */}
+                            <Modal show={show} onHide={handleClose}>
+                                <Modal.Header closeButton>
+                                    <Modal.Title>Inserisci Nuova Esperienza </Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body>
+                                    <Form
+                                        onSubmit={(event) => {
+                                            HandleMakeAPost(event);
+                                        }}
+                                    >
+                                        <Form.Group className="mb-3" controlId="formRuolo">
+                                            <Form.Label>Ruolo:</Form.Label>
+                                            <Form.Control
+                                                type="text"
+                                                placeholder="inserisci ruolo"
+                                                onChange={(event) => {
+                                                    setEsperienzaData({ ...esperienzaData, role: event.target.value });
+                                                }}
+                                            />
+                                        </Form.Group>
+                                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                                            <Form.Label>Company:</Form.Label>
+                                            <Form.Control
+                                                type="text"
+                                                placeholder="inserisci Company"
+                                                onChange={(event) => {
+                                                    setEsperienzaData({
+                                                        ...esperienzaData,
+                                                        company: event.target.value,
+                                                    });
+                                                }}
+                                            />
+                                        </Form.Group>
+                                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                                            <Form.Label>Data Inizio Lavoro</Form.Label>
+                                            <Form.Control
+                                                type="date"
+                                                placeholder="inserisci data inizio lavoro"
+                                                onChange={(event) => {
+                                                    setEsperienzaData({
+                                                        ...esperienzaData,
+                                                        startDate: event.target.value,
+                                                    });
+                                                }}
+                                            />
+                                        </Form.Group>
+                                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                                            <Form.Label>Data Fine Lavoro (non oblbigatoria)</Form.Label>
+                                            <Form.Control
+                                                type="date"
+                                                placeholder="inserisci data termine lavoro"
+                                                onChange={(event) => {
+                                                    setEsperienzaData({
+                                                        ...esperienzaData,
+                                                        endDate: event.target.value,
+                                                    });
+                                                }}
+                                            />
+                                        </Form.Group>
+                                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                                            <Form.Label>Descrizione:</Form.Label>
+                                            <Form.Control
+                                                type="text"
+                                                placeholder="inserisci descrizione"
+                                                onChange={(event) => {
+                                                    setEsperienzaData({
+                                                        ...esperienzaData,
+                                                        description: event.target.value,
+                                                    });
+                                                }}
+                                            />
+                                        </Form.Group>
+                                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                                            <Form.Label>Luogo Di lavoro: </Form.Label>
+                                            <Form.Control
+                                                type="text"
+                                                placeholder="inserisci luodo di lavoro "
+                                                onChange={(event) => {
+                                                    setEsperienzaData({ ...esperienzaData, area: event.target.value });
+                                                }}
+                                            />
+                                        </Form.Group>
+                                        <Modal.Footer>
+                                            <Button
+                                                variant="secondary"
+                                                onClick={() => {
+                                                    handleClose();
+                                                    setEsperienzaData({
+                                                        role: "",
+                                                        company: "",
+                                                        startDate: "",
+                                                        endDate: "", // può essere null
+                                                        description: "",
+                                                        area: "",
+                                                    });
+                                                }}
+                                            >
+                                                Close
+                                            </Button>
+                                            <Button
+                                                type="submit"
+                                                variant="primary"
+                                                onClick={() => {
+                                                    handleClose();
+                                                    setTimeout(() => {
+                                                        setEsperienzaData({
+                                                            role: "",
+                                                            company: "",
+                                                            startDate: "",
+                                                            endDate: "", // può essere null
+                                                            description: "",
+                                                            area: "",
+                                                        });
+                                                    }, 700);
+                                                }}
+                                            >
+                                                Save Changes
+                                            </Button>
+                                        </Modal.Footer>
+                                    </Form>
+                                </Modal.Body>
+                            </Modal>
                         </Row>
                     </Row>
                 </div>
--- react-app/src/redux/functions/fetchPost.js
+++ react-app/src/redux/functions/fetchPost.js
@@ -1,8 +1,19 @@
-export const fetchDataPost = async (option, idUtente, linkEnd) => {
-    const URLPOST = `https://striveschool-api.herokuapp.com/api/profile/${idUtente}/${linkEnd}`;
+import { Token } from "../../token";
+
+export const fetchDataPost = async (url, data) => {
+    /*  const URLPOST = `https://striveschool-api.herokuapp.com/api/profile/${idUtente}/${linkEnd}`; */
+
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${Token} `,
+        },
+        body: JSON.stringify(data),
+    };
 
     try {
-        const response = await fetch(URLPOST, option);
+        const response = await fetch(url, options);
 
         if (!response.ok) {
             throw new Error("qualcosa è andato storto");

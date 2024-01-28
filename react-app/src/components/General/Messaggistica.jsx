e417e6f52d76f265bd517603d7d0d6e49491fc00
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-01-28 19:25:09 +0100

    gg

--- react-app/src/components/General/Messaggistica.jsx
+++ react-app/src/components/General/Messaggistica.jsx
@@ -2,34 +2,59 @@ import { Form, Image, Container, Accordion } from "react-bootstrap";
 import { ThreeDots, PencilSquare } from "react-bootstrap-icons";
 
 const Messaggistica = () => {
-  return (
-    <div className="fixed-bottom w-100 text-end">
-      <Container>
-        <div className="w-50 ms-auto">
-          <Accordion>
-            <Accordion.Item eventKey="0">
-              <Accordion.Header>
-                <div className="d-flex flex-row align-items-center gap-3">
-                  <Image
-                    src="https://w7.pngwing.com/pngs/81/570/png-transparent-profile-logo-computer-icons-user-user-blue-heroes-logo-thumbnail.png"
-                    width={30}
-                    height={30}
-                    roundedCircle
-                  />
-                  Messagistica
-                  <ThreeDots />
-                  <PencilSquare />
+    return (
+        <div className="fixed-bottom w-100 text-end">
+            <Container>
+                <div className="w-50 ms-auto">
+                    <Accordion>
+                        <Accordion.Item eventKey="0">
+                            <Accordion.Header>
+                                <div className="d-flex flex-row align-items-center gap-3">
+                                    <Image
+                                        src="https://w7.pngwing.com/pngs/81/570/png-transparent-profile-logo-computer-icons-user-user-blue-heroes-logo-thumbnail.png"
+                                        width={30}
+                                        height={30}
+                                        roundedCircle
+                                    />
+                                    Messagistica
+                                    <ThreeDots />
+                                    <PencilSquare />
+                                </div>
+                            </Accordion.Header>
+                            <Accordion.Body className="messaggistica-height">
+                                {/*  <Form.Control type="text" placeholder="Cerca.." /> */}
+                                <section className="d-flex w-100">
+                                    <div className="w-25">
+                                        <div className="border">
+                                            <div className="d-flex justify-content-center">
+                                                <p className="m-0 p-3">Chat 1 </p>
+                                            </div>
+                                        </div>
+                                        <div className="border">
+                                            <div className="d-flex justify-content-center">
+                                                <p className="m-0 p-3">Chat 2 </p>
+                                            </div>
+                                        </div>
+                                        <div className="border">
+                                            <div className="d-flex justify-content-center">
+                                                <p className="m-0 p-3">Chat 3 </p>
+                                            </div>
+                                        </div>
+                                        <div className="border">
+                                            <div className="d-flex justify-content-center">
+                                                <p className="m-0 p-3">Chat 4 </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="w-75"></div>
+                                </section>
+                            </Accordion.Body>
+                        </Accordion.Item>
+                    </Accordion>
                 </div>
-              </Accordion.Header>
-              <Accordion.Body className="messaggistica-height">
-                <Form.Control type="text" placeholder="Cerca.." />
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
+            </Container>
         </div>
-      </Container>
-    </div>
-  );
+    );
 };
 
 export default Messaggistica;
